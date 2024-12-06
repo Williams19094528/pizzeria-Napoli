747baa00f7d2befddf7d9c7c6e8b5ff0a74d88ea
Carlos Diaz <carlosdiazbecar@gmail.com>
2024-12-05 21:51:59 -0500

    test

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AppContext } from "../context/AppContext";
 import Cart from "../components/Cart";
 import ProductModal from "../components/ProductModal";
