456e243bfed9bdcd913e9e136b368ad99f8b26f2
Carlos Diaz <carlosdiazbecar@gmail.com>
2024-12-06 16:31:02 -0500

    Modificacion pedido

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -25,7 +25,7 @@ const ShoppingCartPage = () => {
 
   const crearOrden = async (cartItems) => {
     console.log(`carItems: ${cartItems}`);
-    const response = await fetch("https://hito-3-desafio-final-g65.onrender.com/api/pedido",{
+    const response = await fetch("https://hito-3-desafio-final-g65.onrender.com/api/pedidos",{
       method: "POST",
       headers: {
         "Content-Type": "application/json",
